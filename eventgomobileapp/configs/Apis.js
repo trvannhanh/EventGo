5c399fb34a9496b1c62c05b8882344fcc92ac9dc
Nhanh <trvannhanh@gmail.com>
2025-05-16 16:53:05 +0700

    Danh sách Orders, Danh Sách Vé, Tạo Discount

--- eventgomobileapp/configs/Apis.js
+++ eventgomobileapp/configs/Apis.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-export const API_BASE = 'http://192.168.79.102:8000/';
+export const API_BASE = 'http://192.168.79.103:8000/';
 
 export const endpoints = {
     // Auth endpoints
@@ -26,6 +26,7 @@ export const endpoints = {
     discountsCheck: (id) => API_BASE + `events/${id}/check-discount/`,
     payOrder: (id) => API_BASE + `orders/${id}/pay/`,
     checkInTicket: (eventId) => `/events/${eventId}/checkin/`,
+    orders: API_BASE + "orders/",
     
     // User related endpoints
     myTickets: API_BASE + "users/my-tickets/",
